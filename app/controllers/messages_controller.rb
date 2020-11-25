170a86de965629aaa3e41835508041eef7f30dae
Izushi <shoeshinesamurai@gmail.com>
2020-11-25 15:54:27 +0900

    indexの実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,8 @@
 class MessagesController < ApplicationController
+  def index
+    @messages = Message.all
+  end
+
   def new
     @message = Message.new
   end
@@ -8,8 +12,8 @@ class MessagesController < ApplicationController
   end
 
   private
+
   def message_params
     params.require(:message).permit(:title, :content)
   end
-
 end
